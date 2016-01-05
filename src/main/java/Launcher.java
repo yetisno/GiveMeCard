6510fdd1b785cc9c1517b2652a1c57f3b3d75b21
Yeti Sno <yeti@yetiz.org>
2016-01-05 22:11:24 +0800

    init

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,50 @@
+import org.yetiz.utils.CardGenerator;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Random;
+
+/**
+ * Created by yeti on 16/1/5.
+ */
+public class Launcher {
+
+	private static Random random = new Random(System.currentTimeMillis());
+	private static SimpleDateFormat MMYY = new SimpleDateFormat("MMYY");
+	private static Calendar current = Calendar.getInstance();
+
+	public static void main(String... args) {
+		if (args.length != 3) {
+			System.out.println("java -jar givemecard-1.0.jar <[A]mericanExpress, [J]CB, [M]asterCard, [V]isa> <count> <with expire date?[Y/N]>");
+			System.exit(1);
+		}
+		String type = args[0];
+		int count = Integer.parseInt(args[1]);
+		boolean withExpireDate = args[2].equals("Y");
+		if (withExpireDate)
+			System.out.println("    Card No\t Expire Date");
+		else
+			System.out.println("    Card No");
+		for (int i = 0; i < count; i++) {
+			switch (type) {
+				case "A":
+					System.out.print(CardGenerator.AmericanExpress() + (withExpireDate ? "" : "\n"));
+					break;
+				case "J":
+					System.out.print(CardGenerator.JCB() + (withExpireDate ? "" : "\n"));
+					break;
+				case "M":
+					System.out.print(CardGenerator.MasterCard() + (withExpireDate ? "" : "\n"));
+					break;
+				case "V":
+					System.out.print(CardGenerator.Visa() + (withExpireDate ? "" : "\n"));
+					break;
+			}
+			if (withExpireDate) {
+				Calendar calendar = Calendar.getInstance();
+				calendar.set(random.nextInt(10) + current.get(Calendar.YEAR), random.nextInt(12) + 1, 1);
+				System.out.println("\t" + MMYY.format(calendar.getTime()));
+			}
+		}
+	}
+}
---
+++ src/main/java/org/yetiz/utils/CardGenerator.java
@@ -0,0 +1,76 @@
+package org.yetiz.utils;
+
+import java.util.Random;
+
+/**
+ * Created by yeti on 16/1/5.
+ */
+public class CardGenerator {
+	private static Random random = new Random(System.currentTimeMillis());
+
+	public static String getCheckCode(String card) {
+		int sum = 0;
+		for (int i = card.length() - 1; i >= 0; i--) {
+			int current = Integer.parseInt(String.valueOf(card.charAt(i)), 10);
+			current *= (1 + (((card.length() - 1 - i) + 1) % 2));
+			sum += ((current / 10) + (current % 10));
+		}
+		return String.valueOf((10 - (sum % 10)) % 10);
+	}
+
+	public static String JCB() {
+		int length = 16;
+		int firstNumber = random.nextInt(3589 - 3528) + 3528 + 1;
+		StringBuilder stringBuilder = new StringBuilder();
+		for (int i = 0; i < length - 5; i++) {
+			stringBuilder.append(random.nextInt(10));
+		}
+		return stringBuilder
+			.reverse()
+			.insert(0, String.valueOf(firstNumber))
+			.append(getCheckCode(stringBuilder.toString()))
+			.toString();
+	}
+
+	public static String MasterCard() {
+		int length = 16;
+		int firstNumber = random.nextInt(55 - 51) + 51 + 1;
+		StringBuilder stringBuilder = new StringBuilder();
+		for (int i = 0; i < length - 3; i++) {
+			stringBuilder.append(random.nextInt(10));
+		}
+		return stringBuilder
+			.reverse()
+			.insert(0, String.valueOf(firstNumber))
+			.append(getCheckCode(stringBuilder.toString()))
+			.toString();
+	}
+
+	public static String Visa() {
+		int length = 16;
+		int firstNumber = 4;
+		StringBuilder stringBuilder = new StringBuilder();
+		for (int i = 0; i < length - 2; i++) {
+			stringBuilder.append(random.nextInt(10));
+		}
+		return stringBuilder
+			.reverse()
+			.insert(0, String.valueOf(firstNumber))
+			.append(getCheckCode(stringBuilder.toString()))
+			.toString();
+	}
+
+	public static String AmericanExpress() {
+		int length = 15;
+		int firstNumber = random.nextInt(2) == 0 ? 34 : 37;
+		StringBuilder stringBuilder = new StringBuilder();
+		for (int i = 0; i < length - 3; i++) {
+			stringBuilder.append(random.nextInt(10));
+		}
+		return stringBuilder
+			.reverse()
+			.insert(0, String.valueOf(firstNumber))
+			.append(getCheckCode(stringBuilder.toString()))
+			.toString();
+	}
+}
